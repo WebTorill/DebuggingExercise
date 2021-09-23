45738ce80e5654f7486e0e5879b8b13ddd0f6e92
Torill <tosk@eaaa.dk>
2021-09-23 14:19:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DebuggingExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int count = 0;
+
+            while(count <= 1000)
+                if(count % 5 == 1)
+                    sum += sum + count;
+                if(count % 3 == 0)
+                    sum += count;
+
+            // 233168 er svaret vi leder efter
+            Console.WriteLine(sum);
+
+            Console.ReadLine();
+        }
+    }
+}
